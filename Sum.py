d9eedf158943b432303906831b1ee1c6057b0cad
ankit29kothari <33951851+ankit29kothari@users.noreply.github.com>
2017-11-24 23:32:01 +0530

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -2,6 +2,20 @@
 #sum fo the no & print to Console.
 
 
+from __future__ import print_function
+
 print("add 2 nos =")
 print("hello")
 
+
+
+import paho.mqtt.client as paho
+broker="iot.eclipse.org"
+port=1883
+def on_publish(client,userdata,result):
+    print("data published \n")
+pass
+client1= paho.Client("mqtteclipse/cmd")
+client1.on_publish = on_publish
+client1.connect(broker,port)
+ret= client1.publish("mqtteclipse/cmd","on")                  
